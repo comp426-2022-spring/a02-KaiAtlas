8f4600039e0487dfd2a86ea75ed40898382db742
KaiAtlas <zkzhu@ad.unc.edu>
2022-03-26 22:02:39 -0400

    something

--- flips.js
+++ flips.js
@@ -5,7 +5,12 @@ let coins = coinFlips(args[2]);
 let count = countFlips(coins);
 if (args[2] == null){
     coins = coinFlips(1);
-    count = countFlips(coins);
+    if (coins == 'heads'){
+        count = {heads:1};
+    }
+    else{
+        count = {tails:1};
+    }
 }
 console.info(coins);
 console.log(count);
